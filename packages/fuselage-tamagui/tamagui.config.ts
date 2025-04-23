841f906b487f75da36dba8b4a188f375f734c692
Anxhul10 <anxhul10@gmail.com>
2025-04-24 01:52:15 +0530

    added tokens

--- packages/fuselage-tamagui/tamagui.config.ts
+++ packages/fuselage-tamagui/tamagui.config.ts
@@ -1,12 +1,47 @@
-// tamagui.config.ts
 import { defaultConfig } from '@tamagui/config/v4'
-import { createTamagui } from '@tamagui/core'
+import { createTamagui, createTokens } from '@tamagui/core'
 
-export const config = createTamagui(defaultConfig)
+const customTokens = createTokens({
+  size: {
+    ...defaultConfig.tokens.size, // keep default sizes
+    sm: 38,
+    md: 46,
+    lg: 60,
+  },
+  space: {
+    ...defaultConfig.tokens.space,
+    sm: 15,
+    md: 20,
+    lg: 25,
+  },
+  radius: {
+    ...defaultConfig.tokens.radius,
+    sm: 4,
+    md: 8,
+    lg: 12,
+  },
+  // add others if needed...
+})
+
+const customThemes = {
+  ...defaultConfig.themes,
+  light_Button: {
+    ...defaultConfig.themes.light, // inherit from base light theme
+    background: '#ccc',
+    backgroundPress: '#bbb',
+    backgroundHover: '#ddd',
+    color: '#222',
+  },
+}
+
+export const config = createTamagui({
+  ...defaultConfig,
+  tokens: customTokens,
+  themes: customThemes,
+})
 
 type CustomConfig = typeof config
 
-// ensure types work
 declare module 'tamagui' {
   interface TamaguiCustomConfig extends CustomConfig {}
 }
