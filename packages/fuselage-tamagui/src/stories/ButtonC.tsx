3a153bf8d8dfc7a5be6192db40a8af84241e79c1
Anxhul10 <anxhul10@gmail.com>
2025-04-24 01:55:00 +0530

    custom button init

---
+++ packages/fuselage-tamagui/src/stories/ButtonC.tsx
@@ -0,0 +1,9 @@
+import { Button as CButton} from './TamaguiButton'
+
+export const Button = () => {
+    return (
+      <CButton size="$md">
+        <CButton.Text size="$md">Hello world</CButton.Text>
+      </CButton>
+    );
+  };
