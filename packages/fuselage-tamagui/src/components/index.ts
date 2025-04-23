70bdb79b5131810d9e85624aca60da0bba365808
Anxhul10 <anxhul10@gmail.com>
2025-04-23 21:21:00 +0530

    tamagui init

--- packages/fuselage-tamagui/src/components/index.ts
+++
@@ -1,2 +0,0 @@
-// export * from './Button'
-// export {My} from './Button';
--- packages/fuselage-tamagui/vite.config.ts
+++ packages/fuselage-tamagui/vite.config.ts
@@ -1,31 +1,7 @@
-import path from 'path'
-import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import { tamaguiPlugin } from '@tamagui/vite-plugin'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tamaguiPlugin({
-      config: './tamagui.config.ts',
-      components: ['tamagui'],
-      optimize: true,
-    }),
-  ],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'), // or .tsx depending on your entry
-      name: 'FuselageTamagui',
-      fileName: (format) => `fuselage-tamagui.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'tamagui'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
+  plugins: [react()],
 })
