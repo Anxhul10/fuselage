10853af447f896b18261325fea37f424b4377535
Anxhul10 <anxhul10@gmail.com>
2025-05-02 11:07:24 +0530

    updated vite configs for tamagui compatibility

--- packages/fuselage-tamagui/vite.config.ts
+++ packages/fuselage-tamagui/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import { tamaguiPlugin } from '@tamagui/vite-plugin'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  plugins: [
+    react(),
+    tamaguiPlugin({
+      // points to your tamagui config file
+      config: 'src/tamagui.config.ts',
+      // points to any linked packages or node_modules
+      // that have tamagui components to optimize
+      components: ['tamagui'],
+      // turns on the optimizing compiler
+      optimize: true,
+    }),
+  ].filter(Boolean),
+}
