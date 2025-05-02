59460371b198398a906836d297560fd1c41401ac
Anxhul10 <anxhul10@gmail.com>
2025-05-02 11:06:06 +0530

    added decorators

---
+++ packages/fuselage-tamagui/.storybook/preview.tsx
@@ -0,0 +1,26 @@
+import type { Preview } from '@storybook/react'
+import { TamaguiProvider } from 'tamagui'
+import { config } from '../src/tamagui.config'
+
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (  
+    <TamaguiProvider config={config} defaultTheme="light">
+      <Story />
+    </TamaguiProvider>
+    ),
+  ],
+  
+
+};
+
+export default preview;
