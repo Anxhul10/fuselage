a1d48e35dd5cc451b8c1b35c7eb2f368d308fa07
Anxhul10 <anxhul10@gmail.com>
2025-07-01 20:22:58 +0530

    chore: trigger turbosnap

--- packages/fuselage/src/components/Box/Box.tsx
+++ packages/fuselage/src/components/Box/Box.tsx
@@ -7,7 +7,7 @@ import type {
   Ref,
 } from 'react';
 import { createElement, forwardRef, memo } from 'react';
-
+// test to check turbosnap
 import { useArrayLikeClassNameProp } from '../../hooks/useArrayLikeClassNameProp';
 import { useBoxOnlyProps } from '../../hooks/useBoxOnlyProps';
 import type { Falsy } from '../../types/Falsy';
