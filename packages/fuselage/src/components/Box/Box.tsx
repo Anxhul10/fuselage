740b125010bba33b7467de9cedbc94a9f1b610dd
Anxhul10 <anxhul10@gmail.com>
2025-07-03 02:38:01 +0530

    chore: changed files actions init

--- packages/fuselage/src/components/Box/Box.tsx
+++ packages/fuselage/src/components/Box/Box.tsx
@@ -7,7 +7,7 @@ import type {
   Ref,
 } from 'react';
 import { createElement, forwardRef, memo } from 'react';
-
+// some changes
 import { useArrayLikeClassNameProp } from '../../hooks/useArrayLikeClassNameProp';
 import { useBoxOnlyProps } from '../../hooks/useBoxOnlyProps';
 import type { Falsy } from '../../types/Falsy';
